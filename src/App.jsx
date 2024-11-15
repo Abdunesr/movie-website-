b3a243ab4cd39f73b2966e1048cb782378d730ad
Abdunesr <nesredinabdelah33@gmail.com>
2024-11-16 01:26:41 +0300

    adding some reusable props as a Api

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Star from "./Star";
 function App() {
   return (
     <div>
-      <Star maxRating={8} />
+      <Star maxRating={8} size={32} color={"red"} />
+      <Star maxRating={2} size={65} color={"blue"} />
     </div>
   );
 }
--- src/Star.jsx
+++ src/Star.jsx
@@ -1,26 +1,22 @@
 import { useState } from "react";
 
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-};
-
 const container = {
   display: "flex",
   gap: "16px",
   textAlign: "center",
 };
-function Star({ maxRating }) {
-  const [rating, setRating] = useState(1);
+function Star({ maxRating, size, color = "#ffd700" }) {
+  const [rating, setRating] = useState(maxRating);
   const [tempRating, setTempRating] = useState(0);
   function handleRating(rating) {
     setRating(rating);
   }
   return (
     <div style={container}>
-      {Array.from({ length: 7 }, (_, i) => (
+      {Array.from({ length: maxRating }, (_, i) => (
         <Ind
+          size={size}
+          color={color}
           key={i}
           num={i + 1}
           onRating={() => handleRating(i + 1)}
@@ -37,7 +33,12 @@ function Star({ maxRating }) {
   );
 }
 
-function Ind({ full, onRating, onHover, onLeave }) {
+function Ind({ full, onRating, onHover, onLeave, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+  };
   return (
     <div>
       <span
@@ -51,7 +52,7 @@ function Ind({ full, onRating, onHover, onLeave }) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
-            fill="#ffd700"
+            fill={color}
             stroke="#000"
           >
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
